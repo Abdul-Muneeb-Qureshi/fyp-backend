54095f165b5296211ca097f8aec884903591bede
Abdul-Muneeb-Qureshi <115551959+Abdul-Muneeb-Qureshi@users.noreply.github.com>
2025-01-10 17:03:15 +0500

    Add Payment Functionality

--- fypbackend/fypbackend/urls.py
+++ fypbackend/fypbackend/urls.py
@@ -25,7 +25,9 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('users.urls')),
     path('', include('pipeline.urls')),
-    path('', include('contact.urls')),  # Include contact app URLs
+    path('', include('contact.urls')), 
+    path('', include('paymentGateway.urls')), 
+
 
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
